cd9220178df67e5b1233c8bc3f94d8c4ba7657d8
wangtae0320 <85270112+wangtae0320@users.noreply.github.com>
2022-02-09 00:08:37 +0900

    animation test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Box1 from './components/Box1/Box1'
+import Box2 from './components/Box2/Box2'
 
 function App() {
   return (
     <div className="App">
       <Box1 />
+      <Box2 />
     </div>
   )
 }
---
+++ src/components/Box2/Box2.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import Youtube from '../../asset/Youtube.png'
+import {motion} from 'framer-motion'
+
+export default function Box2() {
+
+    const [isAnimating, setIsAnimating] = useState(false)
+     
+    return (
+        <div className="box-container">
+            <motion.img src={Youtube} 
+            alt=""
+            className="image"
+            animate={{
+                x: isAnimating ? 1500 : 0,
+                opacity: isAnimating ? 1 : 0.5,
+                rotate: isAnimating ? 360 : 0
+            }}
+            initial={{
+                opacity: 0.1
+
+            }}
+            transition={{
+                type: "spring",
+                stiffness: 50,
+            }}
+            onClick={() => setIsAnimating(!isAnimating)}
+            >
+            </motion.img>
+        </div>
+
+
+    )
+}
